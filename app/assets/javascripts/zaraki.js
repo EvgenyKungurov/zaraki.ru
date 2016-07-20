2b02348cfbddb7eb7a7a744306b2721c63806892
Evgeny Kungurov <evgenii.kungurov@gmail.com>
2016-07-20 10:03:19 +0900

    Refactored

--- app/assets/javascripts/zaraki.js
+++ app/assets/javascripts/zaraki.js
@@ -1,21 +1,38 @@
 $(document).on('turbolinks:load', function() {
   $('#edit_mode').click(function() {
-    if ($('#edit_mode').attr('checked')) {
-      document.cookie = 'edit_mode = false; path: /';
-    }
-    else {
-      document.cookie = 'edit_mode = true; path: /';
-    }
-
-    if (document.cookie.match('edit_mode = true')) {
-      location.reload();
-      $('a:contains("Edit")').show();
-      $('a:contains("Destroy")').show();
-    }
-    else {
-      location.reload();
-      $('a:contains("Edit")').hide();
-      $('a:contains("Destroy")').hide();
-    }
+    check_edit_mode();
+    show_hide_links();
   });
 });
+
+function check_edit_mode() {
+  if ($('#edit_mode').attr('checked') && document.cookie.match('edit_mode=true')) {
+    disable_edit_mode();
+  }
+  else {
+    enable_edit_mode();
+  }
+}
+
+function disable_edit_mode() {
+  document.cookie = 'edit_mode' + '=;expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+  document.cookie = 'edit_mode = false; path: /';
+}
+
+function enable_edit_mode() {
+  document.cookie = 'edit_mode' + '=;expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+  document.cookie = 'edit_mode = true; path: /';
+}
+
+function show_hide_links() {
+  if (document.cookie.match('edit_mode = true')) {
+    location.reload();
+    $('a:contains("Edit")').show();
+    $('a:contains("Destroy")').show();
+  }
+  else {
+    location.reload();
+    $('a:contains("Edit")').hide();
+    $('a:contains("Destroy")').hide();
+  }
+}
